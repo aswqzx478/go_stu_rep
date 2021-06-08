f38f2136145f87ceece872281335f33b8caff961
jbyf <506921864@qq.com>
2021-06-09 00:20:37 +0800

    2021-06-09 00:20:07 go的常量与变量

--- main.go
+++ main.go
@@ -3,9 +3,14 @@ package main
 import "fmt"
 
 func main() {
-	//-15:向右填充,填充满15个空格,包括字符在内
-	//$4v : 向左填充,不满4 则用空格填充到4个
-	fmt.Printf("%-15v $%4v\n", "SpaceX", "94")
-	//文本右对齐
-	fmt.Printf("%-15v $%4v\n", "Virgin Galactic", 100)
+	const lightSpeed = 299792 // km/s
+	var distance = 56000000   //km
+
+	fmt.Println(distance/lightSpeed, "seconds")
+
+	distance = 401000000
+	fmt.Println(distance/lightSpeed, "seconds")
+
+	//使用const声明常量,
+	//使用var声明变量
 }
